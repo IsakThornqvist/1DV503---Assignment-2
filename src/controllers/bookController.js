6da89f077e59f9b05278e59490f42cd55e0a4e98
Isak <it222hp@student.lnu.se>
2026-01-03 19:42:33 +0100

    update renderBooks to show 5 books at a time with pagination functionality

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,14 +1,41 @@
 import { BookModel } from '../models/bookModel.js'
 
 export class BookController {
+  #bookModel
+  #limit = 5
+
   constructor() {
-    this.bookModel = new BookModel()
+    this.#bookModel = new BookModel()
   }
 
   async renderBooks(req, res, next) {
     try {
-      const books = await this.bookModel.getBooks(10)
-      res.render('books/books', { title: 'Books we offer!', books })
+      const page = parseInt(req.query.page) || 1
+      const offset = (page -1) * this.#limit
+
+      const books = await this.#bookModel.getBooks(this.#limit, offset)
+      const totalBooks = await this.#bookModel.getBookCount()
+
+      const totalPages = Math.ceil(totalBooks / this.#limit)
+
+      const maxPages = 20
+
+      const firstPage = Math.max(1, page - Math.floor(maxPages / 2))
+      const lastPage = firstPage + maxPages - 1
+
+  if (lastPage > totalPages) {
+    lastPage = totalPages
+    firstPage = Math.max(1, lastPage - maxPagesToShow + 1)
+  }
+
+      res.render('books/books', {
+        title: 'Books we offer!',
+        books,
+        currentPage: page,
+        totalPages,
+        firstPage,
+        lastPage
+      })
     } catch (err) {
       next(err)
     }
