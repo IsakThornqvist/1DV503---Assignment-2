fe7540b23cfa492e1f17d58fa75c2e7747223789
Isak <it222hp@student.lnu.se>
2026-01-03 19:40:41 +0100

    add offset for pagination handling and filtering and count to be able to determine how many pages are needed

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -2,11 +2,16 @@ import pool from '../config/db.js'
 
 
 export class BookModel {
-
-async getBooks(limit = 10) {
-  const sql = 'SELECT * FROM books ORDER BY price DESC LIMIT ?'
-  const [rows] = await pool.query(sql, [limit])
+async getBooks(limit = 5, offset = 0) {
+  const sql = 'SELECT * FROM books LIMIT ? OFFSET ?'
+  const [rows] = await pool.query(sql, [limit, offset])
   return rows
 }
+
+  async getBookCount() {
+    const sql = 'SELECT COUNT(*) as total FROM books'
+    const [rows] = await pool.query(sql)
+    return rows[0].total
+  }
 
 }
