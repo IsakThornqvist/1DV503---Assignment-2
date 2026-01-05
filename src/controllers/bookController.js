355965ffb89fa6abc93cd8504ea2f111b0911e48
Isak <it222hp@student.lnu.se>
2026-01-05 13:07:27 +0100

    update controller with subject filtering conditions and update view with data

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -3,7 +3,6 @@ import { BookModel } from '../models/bookModel.js'
 export class BookController {
   #bookModel
   #limit = 5
-
   constructor() {
     this.#bookModel = new BookModel()
   }
@@ -11,12 +10,20 @@ export class BookController {
   async renderBooks(req, res, next) {
     try {
       const page = parseInt(req.query.page) || 1
+      const subject = req.query.subject || null
       const offset = (page -1) * this.#limit
 
-      const books = await this.#bookModel.getBooks(this.#limit, offset)
-      const totalBooks = await this.#bookModel.getBookCount()
-      const subjects = await this.#bookModel.getSubjects()
+      let books
+      let totalBooks
+      if (subject) {
+        books = await this.#bookModel.getBookBySubject(this.#limit, offset, subject)
+        totalBooks = await this.#bookModel.getBookCountBySubject(subject)
+      } else {
+      books = await this.#bookModel.getBooks(this.#limit, offset)
+      totalBooks = await this.#bookModel.getBookCount()
+      }
 
+      const subjects = await this.#bookModel.getSubjects()
       const totalPages = Math.ceil(totalBooks / this.#limit)
 
       const maxPages = 20
@@ -36,7 +43,8 @@ export class BookController {
         totalPages,
         firstPage,
         lastPage,
-        subjects
+        subjects,
+        currentSubject: subject
       })
     } catch (err) {
       next(err)
