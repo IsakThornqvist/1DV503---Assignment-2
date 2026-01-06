28ef9e3d52311ce53f7446114141b2b62980904b
Isak <it222hp@student.lnu.se>
2026-01-07 00:25:55 +0100

    add second eventlistener to handle the urls correctly for two filters at a time

--- src/public/js/books.js
+++ src/public/js/books.js
@@ -3,20 +3,37 @@
  * Adds click event listeners to all elements with the class 'subjectName'.
  * When clicked, redirects the browser to the books page filtered by the selected subject.
  */
+
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.subjectName').forEach(button => {
     button.addEventListener('click', () => {
-      const subject = button.dataset.subject
-      window.location.href = `/books?subject=${encodeURIComponent(subject)}`
+      const clickedSubject = button.dataset.subject
+      console.log(clickedSubject)
+      const urlParamSubject = new URLSearchParams(window.location.search)
+      console.log(urlParamSubject)
+
+      urlParamSubject.set('subject', clickedSubject)
+
+
+      window.location.href = `/books?${urlParamSubject.toString()}`
     })
   })
-})
 
-document.addEventListener('DOMContentLoaded', () => {
+/**
+ * Adds click event listeners to all elements with the class 'authorName'.
+ * When clicked, redirects the browser to the books page filtered by the selected author.
+ */
   document.querySelectorAll('.authorName').forEach(button => {
     button.addEventListener('click', () => {
-      const author = button.dataset.author
-      window.location.href = `/books?author=${encodeURIComponent(author)}`
+      const clickedAuthor = button.dataset.author
+      console.log(clickedAuthor)
+      const urlParamAuthor = new URLSearchParams(window.location.search)
+      console.log(urlParamAuthor)
+
+      urlParamAuthor.set('author', clickedAuthor)
+
+
+      window.location.href = `/books?${urlParamAuthor.toString()}`
     })
   })
 })
