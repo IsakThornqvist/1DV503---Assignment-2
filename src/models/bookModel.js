5dd142af4e897f4acad9b1656183ce03ee6c1bad
Isak <it222hp@student.lnu.se>
2026-01-16 14:14:43 +0100

    add sql queries for each combination of possible searches containing title

--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -105,4 +105,48 @@ async getBooks(limit = 5, offset = 0) {
     
   }
 
+    async getBookByAuthorAndSubjectAndTitle (limit = 5, offset = 0, author = 'none', subject = 'none', title = 'none') {
+    const sql = `SELECT * FROM books WHERE author LIKE ? AND subject = ? AND title LIKE ? LIMIT ? OFFSET ?`
+    const [rows] = await pool.query(sql, [`${author}%`, subject, `%${title}%`,  limit, offset])
+    return rows
+  }
+
+    async getBookCountByAuthorAndSubjectAndTitle(author, subject, title) {
+    const sql = 'SELECT COUNT(*) as total FROM books WHERE author LIKE ? AND subject = ? AND title LIKE ?'
+    const [rows] = await pool.query(sql, [`${author}%`, subject, `%${title}%`])
+    return rows[0].total
+    
+  }
+
+      async getBookByAuthorAndTitle (limit = 5, offset = 0, author = 'none', title = 'none') {
+    const sql = `SELECT * FROM books WHERE author LIKE ? AND title LIKE ? LIMIT ? OFFSET ?`
+    const [rows] = await pool.query(sql, [`${author}%`, `%${title}%`, limit, offset])
+    return rows
+  }
+
+    async getBookCountByTitleAndSubject(title, subject) {
+    const sql = 'SELECT COUNT(*) as total FROM books WHERE title LIKE ? AND subject = ?'
+    const [rows] = await pool.query(sql, [`%${title}%`, subject])
+    return rows[0].total
+    
+  }
+
+    async getBookBySubjectAndTitle (limit = 5, offset = 0, subject = 'none', title = 'none') {
+    const sql = `SELECT * FROM books WHERE author LIKE ? AND title LIKE ? LIMIT ? OFFSET ?`
+    const [rows] = await pool.query(sql, [subject, `%${title}%`, limit, offset])
+    return rows
+  }
+
+  async getBookByTitle (limit = 5, offset = 0, title = 'none') {
+    const sql = 'SELECT * FROM books WHERE title LIKE ? LIMIT ? OFFSET ?'
+    const [rows] = await pool.query(sql, [`%${title}%`, limit, offset])
+    return rows
+  }
+
+    async getBookCountByTitle(title) {
+    const sql = 'SELECT COUNT(*) as total FROM books WHERE title LIKE ?'
+    const [rows] = await pool.query(sql, [`%${title}%`])
+    return rows[0].total
+    
+  }
 }
