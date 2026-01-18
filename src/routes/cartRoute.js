9a1881a78b1659432aaf1be3845ad9bd2f3c8e5d
Isak <it222hp@student.lnu.se>
2026-01-18 14:20:51 +0100

    update router with cart routes and add a cart button in the header

--- src/routes/cartRoute.js
+++ src/routes/cartRoute.js
@@ -4,4 +4,14 @@ import { CartController } from '../controllers/cartController.js'
 export const router = express.Router()
 const controller = new CartController()
 
-router.get()
+router.get('/', (req, res, next) => controller.renderCart(req, res, next)
+)
+
+router.post('/clear', (req, res, next) => controller.clearCart(req, res, next)
+)
+
+router.post('/add', (req, res, next) => controller.addToCart(req, res, next)
+)
+router.post('/checkout', (req, res, next) => controller.checkout(req, res, next))
+
+
--- src/routes/router.js
+++ src/routes/router.js
@@ -5,6 +5,7 @@ import { router as homeRouter } from './homeRouter.js'
 import { router as registerRouter } from './registerRouter.js'
 import { router as loginRouter } from './loginRouter.js'
 import { router as bookRouter } from './bookRouter.js'
+import { router as cartRouter } from './cartRoute.js'
 
 
 export const router = express.Router()
@@ -13,6 +14,7 @@ router.use('/', homeRouter)
 router.use('/register', registerRouter)
 router.use('/login', loginRouter)
 router.use('/books', bookRouter)
+router.use('/cart', cartRouter)
 
 
 
