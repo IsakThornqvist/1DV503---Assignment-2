889e21a1c939905159a9a735a353bb58a67d4662
Isak <it222hp@student.lnu.se>
2026-01-18 15:26:09 +0100

    updated implementation of cartController and the cart.ejs

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,10 +1,72 @@
-import { CartModel } from "../models/cartModel.js"
+import { CartModel } from '../models/cartModel.js'
+import { OrderModel } from '../models/orderModel.js'
+import { MemberModel } from '../models/memberModel.js'
 
 export class CartController {
-    #cartModel
+  #cartModel
 
 
-    constructor() {
-        this.#cartModel = new CartModel()
+  constructor() {
+    this.#cartModel = new CartModel()
+
+  }
+
+  async renderCart(req, res, next) {
+    try {
+      if (!req.session.user) {
+        return res.redirect('/login')
+      }
+
+      const userid = req.session.user.id
+      const allCartItems = await this.#cartModel.getCartItemsWithUserId(userid)
+
+      const cartTotalAmount = allCartItems.reduce(
+        (sum, item) => sum + Number(item.total),
+        0
+      )
+
+      res.render('cart/cart', {
+        title: 'Your Cart',
+        allCartItems,
+        cartTotalAmount
+      })
+    } catch (err) {
+      next(err)
     }
+  }
+
+  async clearCart(req, res, next) {
+    try {
+      if (!req.session.user) {
+        return res.redirect('/login')
+      }
+
+      const userid = req.session.user.id
+      await this.#cartModel.clearCart(userid)
+
+      res.redirect('/cart')
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  async addToCart(req, res, next) {
+    try {
+      if (!req.session.user) {
+        return res.redirect('/login')
+      }
+
+      const userid = req.session.user.id
+      const { isbn, qty } = req.body
+
+      await this.#cartModel.addBookToCart(userid, isbn, Number(qty) || 1)
+
+      res.redirect('/cart')
+    } catch (err) {
+      next(err)
+    }
+  }
+
+
+
 }
